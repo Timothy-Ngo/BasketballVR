9f67683fada66e527baaedd8fcf955d464fc604b
Nathaniel-Leslie2 <64575673+Nathaniel-Leslie2@users.noreply.github.com>
2024-05-06 11:35:39 -0700

    Added Scoring to My Scene Folder
    - Transparent PickUp Object
    - Canvas for Text
    - Basketball score script in script folder
    - Now, add script to basketball object and place the pickup object in the center/bottom of each net to simulate scoring

---
+++ Assets/Scenes/Nathaniels Folder/Scripts/BasketBallScore.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    // Rigidbody of the ball
+    private Rigidbody rb;
+
+    // Variable to keep track of collected "PickUp" objects.
+    private int count;
+
+    // UI text component to display count of "PickUp" objects collected.
+    public TextMeshProUGUI countText;
+
+    // UI object to display winning text.
+    public GameObject winTextObject;
+
+    // Start is called before the first frame update.
+    void Start()
+    {
+        // Get and store the Rigidbody component attached to the ball
+        rb = GetComponent<Rigidbody>();
+
+        // Initialize count to zero.
+        count = 0;
+
+        // Update the count display.
+        SetCountText();
+
+        // Initially set the win text to be inactive.
+        winTextObject.SetActive(false);
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Check if the object the player collided with has the "PickUp" tag.
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+
+            // Increment the count of "PickUp" objects collected.
+            count = count + 1;
+
+            // Update the count display.
+            SetCountText();
+        }
+    }
+
+    // Function to update the displayed count of "PickUp" objects collected.
+    void SetCountText()
+    {
+        // Update the count text with the current count.
+        countText.text = "Score: " + count.ToString();
+
+    }
+}
+
